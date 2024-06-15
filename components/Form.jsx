65ad01b42a74911e35fe45e5ad9891e321310dc8
asanthosh200210@gmail.com <asanthosh200210@gmail.com>
2024-06-15 20:08:07 +0530

    added

---
+++ components/Form.jsx
@@ -0,0 +1,61 @@
+// "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=0&longitude=0"
+
+
+
+
+import { useState } from "react";
+import { convertToEmoji } from "./utils";
+import styles from "./Form.module.css";
+import { useNavigate } from "react-router-dom";
+
+function Form() {
+  const [cityName, setCityName] = useState("");
+
+  const [date, setDate] = useState(new Date());
+  const [notes, setNotes] = useState("");
+  const [country,setCountry] = useState();
+const navigate=useNavigate();
+  
+  return (
+    <form className={styles.form}>
+      <div className={styles.row}>
+        <label htmlFor="cityName">City name</label>
+        <input
+          id="cityName"
+          onChange={(e) => setCityName(e.target.value)}
+          value={cityName}
+        />
+        {/* <span className={styles.flag}>{emoji}</span> */}
+      </div>
+
+      <div className={styles.row}>
+        <label htmlFor="date">When did you go to {cityName}?</label>
+        <input
+          id="date"
+          onChange={(e) => setDate(e.target.value)}
+          value={date}
+        />
+      </div>
+
+      <div className={styles.row}>
+        <label htmlFor="notes">Notes about your trip to {cityName}</label>
+        <textarea
+          id="notes"
+          onChange={(e) => setNotes(e.target.value)}
+          value={notes}
+        />
+      </div>
+
+      <div className={styles.buttons}>
+        <Button>Add</Button>
+        <Button onClick={(e)=>{
+          e.preventDefault();
+          navigate(-1);
+          
+        }}>&larr; Back</Button>
+      </div>
+    </form>
+  );
+}
+
+export default Form;
